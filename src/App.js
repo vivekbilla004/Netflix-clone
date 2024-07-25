cf08b4fa739a3384473e9b794132eb7e0f12cf7e
vivek <vivekbilla822@gmail.com>
2024-07-26 00:19:04 +0530

    SignIn and Signup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-green-500 font-bold text-2xl">
+    <div className="App text-green-500 font-bold text-2xl ">
      Hello Everyone Welcome to netflix - Gpt
     </div>
   );
